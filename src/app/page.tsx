912cb8608687e3b3c2e61fe0ba9fdebb9e9af4a4
Truc Nguyen <truc.nguyentrung@hotmail.com>
2024-03-16 10:17:13 +0000

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,198 @@
-import Image from "next/image";
+"use client";
+
+import { UserButton, useAuth, useUser } from "@clerk/nextjs";
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+import cn from "classnames";
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useMemo, useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Skeleton } from "@/components";
+
+dayjs.extend(weekday);
+
+function Body() {
+  const supabase = createClient();
+  const [players, setPlayers] = useState<any[] | null>();
+  const { userId } = useAuth();
+  const { user } = useUser();
+  const playerId = useMemo(() => {
+    return players?.find((p) => p.clerk_id == userId)?.id;
+  }, [players, userId]);
+
+  const weekStart = dayjs().weekday(8);
+  const weekEnd = weekStart.add(7);
+  const weekDays = [0, 1, 2, 3, 4, 5, 6].map((item) => {
+    const d = weekStart.add(item, "day");
+    return d;
+  });
+  const [pageLoading, setPageLoading] = useState(false);
+
+  useEffect(() => {
+    supabase
+      .channel("reg")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "player_registrations" },
+        handlePlayerRegistrations
+      )
+      .on(
+        "postgres_changes",
+        { event: "DELETE", schema: "public", table: "player_registrations" },
+        handlePlayerRegistrations
+      )
+      .subscribe();
+  }, []);
+
+  useEffect(() => {
+    setPageLoading(true);
+    if (user?.fullName) {
+      addPlayerIfNotExists().then(() => {
+        setPageLoading(false);
+      });
+    }
+  }, [user, user?.fullName]);
+
+  function handlePlayerRegistrations(payload: any) {
+    console.log(payload);
+  }
+
+  async function getPlayers() {
+    const players = await supabase.from("players").select(`
+      id,
+      name,
+      clerk_id,
+      player_registrations (
+        id,
+        week_day
+      )
+    `);
+    return players.data;
+  }
+
+  async function addPlayerIfNotExists() {
+    const players = await getPlayers();
+    if (!players?.find((p) => p.clerk_id === userId)) {
+      try {
+        await supabase
+          .from("players")
+          .insert({
+            clerk_id: userId,
+            name: user!.fullName,
+          })
+          .select();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setPlayers(players);
+  }
+
+  async function handleSelectDay(d: dayjs.Dayjs, registrationId?: number) {
+    try {
+      if (registrationId) {
+        await supabase
+          .from("player_registrations")
+          .delete()
+          .eq("id", registrationId);
+        toast(`Not comming on ${d.format("ddd DD")}`);
+      } else {
+        await supabase.from("player_registrations").insert({
+          player_id: playerId,
+          week_day: d.toDate(),
+        });
+        toast(`Come on ${d.format("ddd DD")}`);
+      }
+
+      const data = await getPlayers();
+      setPlayers(data);
+    } catch (err) {
+      console.log(err);
+      toast.error(
+        "Oops! can not register now, contact developer for fixing this shit!"
+      );
+    } finally {
+    }
+  }
+
+  if (pageLoading) {
+    return (
+      <div className="bg-white flex flex-col gap-2">
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center p-1 gap-3 my-3">
+      <span>{players?.length ?? 0} players</span>
+      <div className="flex flex-col justify-start items-start w-full gap-1">
+        {players &&
+          players.map((player, i) => {
+            const mine = userId == player.clerk_id;
+            return (
+              <div
+                key={i}
+                className="flex items-center gap-1 w-full h-10 text-xs"
+              >
+                <div className="w-14 h-full flex items-center overflow-hidden text-ellipsis">
+                  {mine ? (
+                    <div className="w-10 h-10 bg-slate-100 text-white flex items-center justify-center rounded-full">
+                      <UserButton defaultOpen={false} />
+                    </div>
+                  ) : (
+                    <div className="w-10 h-10 bg-slate-100 flex items-center justify-center rounded-full relative">
+                      {player.name.split(" ")[0][0]}
+                      {player.name.split(" ")[1][0]}
+                      <span className="w-3 h-3 bg-green-500 rounded-full top-1 -right-1 absolute"></span>
+                    </div>
+                  )}
+                </div>
+
+                <div className="h-full grid grid-cols-7 gap-1 flex-1">
+                  {weekDays.map((day, j) => {
+                    const registration = player.player_registrations
+                      .map((r: any) => ({ day: r.week_day, id: r.id }))
+                      .find(
+                        (d: any) => dayjs(d.day).weekday() == day.weekday()
+                      );
+                    const isBooked = !!registration;
+
+                    return (
+                      <button
+                        disabled={!mine}
+                        onClick={() => handleSelectDay(day, registration?.id)}
+                        className={cn(
+                          "h-full flex text-center justify-center items-center w-full rounded-lg ring-green-500",
+                          {
+                            "active:ring-2 active:ring-green-500 active:ring-offset-2":
+                              mine,
+                            "bg-green-100": mine && !isBooked,
+                            "bg-slate-50": !mine && !isBooked,
+                            "bg-green-500 text-white font-bold":
+                              mine && isBooked,
+                            "bg-green-500/50 text-white font-bold":
+                              !mine && isBooked,
+                          }
+                        )}
+                        key={j}
+                      >
+                        {day.format("ddd")}
+                      </button>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  return <Body />;
 }
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { Skeleton as Skeleton } from '@/components/skeleton'
